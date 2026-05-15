e66217c746ad7d1080023ecf6865a02b7fb94bd4
ZastinMYvis <aldialiqsa@gmail.com>
2026-05-15 19:35:45 +0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "love the PBB community❤️",
+        name: "Love the PBB community❤️",
         // Activity type number (0 = Playing).
         type: 0, 
       },
